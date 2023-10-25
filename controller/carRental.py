48bf4bac154ecb6bfe9a920ed06056acbb67d4f6
ErikkaSteen <erioas@hotmail.no>
2023-10-25 11:47:50 +0200

    SafeCopy

--- controller/carRental.py
+++ controller/carRental.py
@@ -1,20 +1,21 @@
 from project import app
 from flask import render_template, request, redirect, url_for 
-from Oblig4.models.cars import *
+from model.cars import *
+from model.customer import *
+from model.employee import *
 
-@app.route('/save_car', methods=["POST"]) 
-
-def save_car_info():    
+###CARS
+@app.route('/create_car', methods=["POST"]) 
+def create_car_info():    
     record = json.loads(request.data)
     print(record)
-    return save_car(record['make'], record['model'], record['reg'], record['year'], record['capacity'])
-
+    return save_car(record['make'], record['model'], record['reg'], record['year'], record['status'], record['location'] )
 
 @app.route('/update_car', methods=['PUT'])
 def update_car_info():
     record = json.loads(request.data)
     print(record)
-    return update_car(record['make'], record['model'], record['reg'], record['year'], record['capacity'])
+    return update_car(record['make'], record['model'], record['reg'], record['year'], record['status'], record['location'])
 
 @app.route('/delete_car', methods=['DELETE'])
 def delete_car_info():
@@ -23,3 +24,61 @@ def delete_car_info():
     delete_car(record['reg'])
     return findAllCars()
 
+@app.route('/read_car', method=['GET'])
+def read_car_info():
+    record = json.loads(request.data)
+    print(record)
+    return findCarByReg(record['reg'])
+
+####CUSTOMER
+@app.route('/create_customer', methods=["POST"]) 
+def create_customer_info():    
+    record = json.loads(request.data)
+    print(record)
+    return create_customer(record['name'], record['age'], record['address'])
+
+@app.route('/update_customer', methods=['PUT'])
+def update_customer_info():
+    record = json.loads(request.data)
+    print(record)
+    return update_customer(record['name'], record['age'], record['address'])
+
+@app.route('/delete_customer', methods=['DELETE'])
+def delete_customer_info():
+    record = json.loads(request.data) 
+    print(record) 
+    delete_customer(record['name'])
+    return findAllCustomers()
+
+@app.route('/read_customer', method=['GET'])
+def read_customer_info():
+    record = json.loads(request.data)
+    print(record)
+    return findCustomerByName(record['name'])
+
+
+###EMPLOYEE
+@app.route('/create_employee', methods=["POST"]) 
+def create_employee_info():    
+    record = json.loads(request.data)
+    print(record)
+    return create_employee(record['name'], record['address'], record['branch'])
+
+@app.route('/update_employee', methods=['PUT'])
+def update_employee_info():
+    record = json.loads(request.data)
+    print(record)
+    return update_employee(record['name'], record['address'], record['branch'])
+
+@app.route('/delete_employee', methods=['DELETE'])
+def delete_employee_info():
+    record = json.loads(request.data) 
+    print(record) 
+    delete_employee(record['name'])
+    return findAllEmployees()
+
+@app.route('/read_employee', method=['GET'])
+def read_employee_info():
+    record = json.loads(request.data)
+    print(record)
+    return findEmployeeByName(record['name'])
---
+++ model/customer.py
@@ -0,0 +1,72 @@
+from neo4j import GraphDatabase, Driver, AsyncGraphDatabase, AsyncDriver
+import re
+
+
+URI = "neo4j+s://c914a714.databases.neo4j.io"
+AUTH = ("neo4j", "Z2NG0YLPs6UTaiFNIN5zn1IcY1smIoWpaYgaa8t1IFc")
+
+
+def _get_connection() -> Driver:
+    driver = GraphDatabase.driver(URI, auth=AUTH)
+    driver.verify_connectivity()
+    return driver
+
+def findAllCustomers():
+    with _get_connection().session() as session:
+        customers = session.run("MATCH (a:Customer) RETURN a;")
+        nodes_json = [node_to_json(record["a"]) for record in customers] 
+        print(nodes_json)
+        return nodes_json
+
+def findCustomerByName(name):
+    with _get_connection().session() as session:
+        customers = session.run("MATCH (a:Customer) where a.name=$name RETURN a;", name=name)    
+        print(customers)
+        nodes_json = [node_to_json(record["a"]) for record in customers]
+    print(nodes_json)
+
+def create_customer(name, age, address):
+    customers = _get_connection().execute_query("MERGE (a:Customer{name: $name, age: $age, address: $address}) RETURN a;", 
+        name=name, age=age, address=address)
+    nodes_json = [node_to_json(record["a"]) for record in customers] 
+    print(nodes_json)
+    return nodes_json
+
+def update_customer(name, age, address): 
+    with _get_connection().session() as session:
+        customers = session.run("MATCH (a:Customer{name:$name}) set a.name=$name, a.age=$age, a.address = $address RETURN a;", 
+            name=name, age=age, address=address) 
+        print(customers)
+        nodes_json = [node_to_json(record["a"]) for record in customers] 
+        print(nodes_json)
+        return nodes_json
+
+def delete_customer(name):
+    _get_connection().execute_query("MATCH (a:Customer{name: $name}) delete a;", name = name)
+
+class Customer:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def get_CustomerName(self):
+        return self.name
+
+    def set_CustomerName(self, value):
+        self.name = value
+
+    def get_CustomerAge(self):
+        return self.age
+
+    def set_CustomerAge(self, value):
+        self.age = value
+
+    def get_CustomerAddress(self):
+        return self.address
+
+    def set_CustomerAddress(self, value):
+        self.address = value
+
+
+
